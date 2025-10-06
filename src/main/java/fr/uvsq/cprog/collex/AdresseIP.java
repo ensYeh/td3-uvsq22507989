db031540e8dcf1e0b700840b5b91d2ee875e60bf
uvsq22507989 <sarah.belmokhtr@gmail.com>
2025-10-06 12:24:18 +0200

    ajout des classes AdresseIP NomMachine DnsItem

---
+++ src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
@@ -0,0 +1,24 @@
+package fr.uvsq.cprog.collex;
+
+import java.util.Objects;
+
+   private final String ip;
+
+    public Adress
+
+      throw new IllegalArgumentException("IP ne pString[]parts=ip.split("\\.");if parts.lent != 4)throw nwIllegalArgumentExceping p : parts) {        it v 
+
+        } catch (Numbr        th 
+
+    this. }
+
+    return ip;@
+
+    if (t     if (!(o instanceo AdresseIP adresseIP = (A 
+
+public in     return Objects.hash(p
+
+ public String toString() {
+        return ip;
+    }
+}
---
+++ src/main/java/fr/uvsq/cprog/collex/NomMachine.java
@@ -0,0 +1,45 @@
+package dns;
+
+import java.util.Objects;
+
+public class NomMachine {
+   private final String nomQualifie;
+
+   public NomMachine(String nomQualifie) {
+      if (!nomQualifie.matches("^[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)+$"))
+         throw new IllegalArgumentException("Nom machine invalide : " + nomQualifie);
+      this.nomQualifie = nomQualifie;
+   }
+
+   public String getNom() {
+      return nomQualifie.split("\\.")[0];
+   }
+
+   public String getDomaine() {
+      return nomQualifie.substring(nomQualifie.indexOf('.') + 1);
+   }
+
+   public String getNomQualifie() {
+      return nomQualifie;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o)
+         return true;
+      if (!(o instanceof NomMachine))
+         return false;
+      NomMachine that = (NomMachine) o;
+      return nomQualifie.equals(that.nomQualifie);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(nomQualifie);
+   }
+
+   @Override
+   public String toString() {
+      return nomQualifie;
+   }
+}
